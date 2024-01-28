ff94cb9020f9fd6310e5687a4da9d578a5b4ba22
sonny_developer <66192341+JuhyungSon@users.noreply.github.com>
2024-01-28 15:31:41 +0800

    Update HandleCheck.cs

--- HandleCheck.cs
+++ HandleCheck.cs
@@ -8,21 +8,19 @@ public class HandleCheck : MonoBehaviour
     public GameObject Sucess;
     public GameObject tino;
     public GameObject v6Defence;
-   // public GameObject timeChecker;
+ 
    
  
     // Start is called before the first frame update
     void Start()
     {
-        
-       // UnityEngine.Debug.Log(System.DateTime.Now.ToLongTimeString() + " : ");
-        //   handleChecker.transform.position = new Vector3(1.442f,0.72f,4.31f);
+     
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-       // UnityEngine.Debug.Log(System.DateTime.Now.ToLongTimeString() + " : ");
+      
         if (handleChecker.transform.rotation.z < 0.1f)
         {
             Sucess.SetActive(true);
@@ -30,8 +28,7 @@ public class HandleCheck : MonoBehaviour
           
             v6Defence.SetActive(false);
             
-           // Debug.Log("Success");
-            //Debug.Log(Time.time);
+          
             UnityEngine.Debug.Log(System.DateTime.Now.ToLongTimeString() + " : ");
             Debug.Log("User Success valve task time");
         }
